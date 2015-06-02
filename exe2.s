498eb228b3763e461f5b6195e5d0aca8469ce6f4
jeukel <jeukel@gmail.com>
2015-06-01 21:56:05 -0600

    Exercice #2. Need tweaking.

---
+++ exe2.s
@@ -0,0 +1,56 @@
+	.section	.rodata
+.LC0:
+	.string	"Please enter a number: "
+.LC1:
+	.string	"%lf"
+.LC2:
+	.string	"The square root is: %f \n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movq	-24(%rbp), %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	call	sqrt
+	movq	%xmm0, %rax
+	movq	%rax, -16(%rbp)
+	movq	-16(%rbp), %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	movl	$.LC2, %edi
+	movl	$1, %eax
+	call	printf
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L3
+	call	__stack_chk_fail
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.9.2-10ubuntu13) 4.9.2"
+	.section	.note.GNU-stack,"",@progbits
