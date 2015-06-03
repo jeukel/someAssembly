d823fa22e73d617890b267a8d0ae1b58fbfd8373
jeukel <jeukel@gmail.com>
2015-06-03 16:05:39 -0600

    Cleaning code.

--- exe2.s
+++ exe2.s
@@ -1,4 +1,3 @@
-	.section	.rodata
 .LC0:
 	.string	"Please enter a number: "
 .LC1:
@@ -10,12 +9,8 @@
 	.type	main, @function
 main:
 .LFB0:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	subq	$48, %rsp
 	movq	%fs:40, %rax
 	movq	%rax, -8(%rbp)
@@ -47,10 +42,6 @@ main:
 	call	__stack_chk_fail
 .L3:
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.9.2-10ubuntu13) 4.9.2"
-	.section	.note.GNU-stack,"",@progbits
