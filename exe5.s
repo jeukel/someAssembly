aff9c94f41d504e4ab99738b7ed44642ff774e06
zyoruk <luisangel1993julio@gmail.com>
2015-06-05 16:07:48 -0600

    1) Commented the code. 2) Added the file that is read.

--- exe5.s
+++ exe5.s
@@ -1,16 +1,16 @@
 .LC0:
-	.string	"r+"
+	.string	"r+" #String que se utiliza para abrir un archivo para lectura
 .LC1:
-	.string	"file.txt"
+	.string	"file.txt" #Archivo que vamos a leer
 .LC2:
-	.string	"%s"
+	.string	"%s" #String que se utiliza para formateo
 .LC3:
-	.string	"w+"
+	.string	"w+" #String que se utiliza para crear un archivo
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB0: #Abrimos el archivo, declaramos un entero
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -24,7 +24,7 @@ main:
 	movq	%rax, -24(%rbp)
 	movl	$0, -4(%rbp)
 	jmp	.L2
-.L3:
+.L3: #Leemos y printemos 
 	movq	-24(%rbp), %rdx
 	leaq	-80(%rbp), %rax
 	movq	%rdx, %rcx
@@ -38,9 +38,9 @@ main:
 	movl	$0, %eax
 	call	printf
 	addl	$1, -4(%rbp)
-.L2:
+.L2:#Bloque para enciclar la lectura y se encarga de crear variables
 	cmpl	$40, -4(%rbp)
-	jle	.L3
+	jle	.L3  #Llama al bloque para printear
 	movq	-24(%rbp), %rax
 	movq	%rax, %rdi
 	call	fclose
@@ -51,7 +51,7 @@ main:
 	movl	$0, -4(%rbp)
 	movl	$1640, -8(%rbp)
 	jmp	.L4
-.L6:
+.L6: #Tomamos caracter, comparamos con asterisco
 	movq	-24(%rbp), %rax
 	movq	%rax, %rdi
 	call	fgetc
@@ -59,9 +59,9 @@ main:
 	cmpb	$42, -25(%rbp)
 	jne	.L5
 	addl	$1, -4(%rbp)
-.L5:
+.L5:#Removemos 1 al contador
 	subl	$1, -8(%rbp)
-.L4:
+.L4:#bloque que crea un  ciclo y si no entra al cilo cierra y abre
 	cmpl	$0, -8(%rbp)
 	jne	.L6
 	movq	-24(%rbp), %rax
@@ -73,7 +73,7 @@ main:
 	movq	%rax, -24(%rbp)
 	movl	$0, -12(%rbp)
 	jmp	.L7
-.L9:
+.L9:#Bloque que que escribe una nueva linea si esta al final
 	cmpl	$39, -12(%rbp)
 	jne	.L8
 	movq	-24(%rbp), %rax
@@ -82,14 +82,14 @@ main:
 	call	fputc
 	movl	$0, -12(%rbp)
 	jmp	.L7
-.L8:
+.L8:#Bloque que escribe asterisco en el archivo.
 	movq	-24(%rbp), %rax
 	movq	%rax, %rsi
 	movl	$42, %edi
 	call	fputc
 	addl	$1, -12(%rbp)
 	subl	$1, -4(%rbp)
-.L7:
+.L7:# bloque final que crea un ciclo y si no , cierra y termina.
 	cmpl	$0, -4(%rbp)
 	jne	.L9
 	movq	-24(%rbp), %rax
