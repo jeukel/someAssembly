987c25a7892b1247aba65145291c60053ddd970d
Bryan Rodriguez <bryan37_33@hotmail.com>
2015-06-05 21:53:35 -0600

    Oops.

--- exe3.s
+++ exe3.s
@@ -1,5 +1,3 @@
-	.file	"Arbol.c"
-	.section	.rodata
 .LC0:
 	.string	"Ingrese la altura del arbol: "
 .LC1:
@@ -9,12 +7,8 @@
 	.type	main, @function
 main:
 .LFB0:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	subq	$16, %rsp
 	movl	$.LC0, %edi
 	movl	$0, %eax
@@ -46,10 +40,6 @@ main:
 	jle	.L5
 	movl	$0, %eax
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
-	.section	.note.GNU-stack,"",@progbits
